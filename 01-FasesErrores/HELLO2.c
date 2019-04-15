db968ebc2c2091b6a96303246048d55c67b3a6aa
jenniferdeisy <deisy.jennifer@gmail.com>
2019-04-14 23:23:00 -0300

    Add files via upload

---
+++ 01-FasesErrores/HELLO2.c
@@ -0,0 +1,10 @@
+/*
+Hello Word
+Muestra por salida de pantalla la palabra Hello Word
+Autor : Deisy Escobar Cabrera
+Fecha Ultima Modificacion : 14/04/2019
+*/
+#include <stdio.h>
+int/*medio*/main(void){
+int i=42;
+ prontf("La respuesta es %d\n");
